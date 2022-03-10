b901f4df5c691670aace9313e0d6b089f212f03f
riccio <riccio82@gmail.com>
2022-03-10 11:12:03 +0100

    Fix: splitted segments

--- static/src/js/components/review_improved/review.core_components_extension.js
+++ static/src/js/components/review_improved/review.core_components_extension.js
@@ -1,156 +1,218 @@
+;(function () {
+  let ReviewImprovedSideButton =
+    require('./ReviewImprovedTranslationIssuesSideButton').default
 
-(function() {
-    let ReviewImprovedSideButton = require( './ReviewImprovedTranslationIssuesSideButton' ).default;
+  function overrideTranslationIssuesSideButton(TranslationIssuesSideButton) {
+    TranslationIssuesSideButton.prototype.render = function () {
+      return <ReviewImprovedSideButton {...this.props} />
+    }
+  }
 
-    function overrideTranslationIssuesSideButton( TranslationIssuesSideButton ) {
-        TranslationIssuesSideButton.prototype.render = function () {
-            return <ReviewImprovedSideButton {...this.props}/>
-        }
+  function overrideSegmentBodyFunctions(SegmentBody) {
+    SegmentBody.prototype.getStatusMenu = function () {
+      if (this.state.showStatusMenu) {
+        return (
+          <ul
+            className="statusmenu"
+            ref={(menu) => (this.statusMenuRef = menu)}
+          >
+            <li className="arrow">
+              <span className="arrow-mcolor" />
+            </li>
+
+            <li>
+              <a
+                className="draftStatusMenu"
+                data-sid={'segment-' + this.props.segment.sid}
+                title="set draft as status"
+                onClick={this.changeStatus.bind(this, 'draft')}
+              >
+                DRAFT
+              </a>
+            </li>
+            <li>
+              <a
+                className="translatedStatusMenu"
+                data-sid={'segment-' + this.props.segment.sid}
+                title="set translated as status"
+                onClick={this.changeStatus.bind(this, 'translated')}
+              >
+                TRANSLATED
+              </a>
+            </li>
+            <li>
+              <a
+                className="approvedStatusMenu"
+                data-sid={'segment-' + this.props.segment.sid}
+                title="set approved as status"
+                onClick={this.changeStatus.bind(this, 'approved')}
+              >
+                APPROVED
+              </a>
+            </li>
+
+            {!ReviewImproved && config.reviewType !== 'improved' ? (
+              <li>
+                <a
+                  className="rejectedStatusMenu"
+                  data-sid={'segment-' + this.props.segment.sid}
+                  title="set rejected as status"
+                  onClick={this.changeStatus.bind(this, 'rejected')}
+                >
+                  REJECTED
+                </a>
+              </li>
+            ) : null}
+
+            {ReviewImproved || config.reviewType == 'improved' ? (
+              <li>
+                <a
+                  className="fx"
+                  data-sid={'segment-' + this.props.segment.sid}
+                  title="set fixed as status"
+                  onClick={this.changeStatus.bind(this, 'fixed')}
+                >
+                  FIXED
+                </a>
+              </li>
+            ) : null}
+            {ReviewImproved || config.reviewType == 'improved' ? (
+              <li>
+                <a
+                  className="rb"
+                  data-sid={'segment-' + this.props.segment.sid}
+                  title="set rebutted as status"
+                  onClick={this.changeStatus.bind(this, 'rebutted')}
+                >
+                  REBUTTED
+                </a>
+              </li>
+            ) : null}
+          </ul>
+        )
+      } else {
+        return ''
+      }
     }
 
-    function overrideSegmentBodyFunctions( SegmentBody ) {
-        SegmentBody.prototype.getStatusMenu = function () {
-            if ( this.state.showStatusMenu ) {
-                return <ul className="statusmenu" ref={( menu ) => this.statusMenuRef = menu}>
-                    <li className="arrow"><span className="arrow-mcolor"/></li>
+    const originalbeforeRender = SegmentBody.prototype.beforeRenderOrUpdate
+    SegmentBody.prototype.beforeRenderOrUpdate = function () {
+      if (!(ReviewImproved && ReviewImproved.enabled() && Review.enabled())) {
+        originalbeforeRender.apply(this)
+      }
+    }
+    const originalAfterRender = SegmentBody.prototype.afterRenderOrUpdate
+    SegmentBody.prototype.afterRenderOrUpdate = function () {
+      if (!(ReviewImproved && ReviewImproved.enabled() && Review.enabled())) {
+        originalAfterRender.apply(this)
+      }
+    }
+  }
 
-                    <li>
-                        <a className="draftStatusMenu" data-sid={"segment-" + this.props.segment.sid} title="set draft as status"
-                           onClick={this.changeStatus.bind( this, 'draft' )}>
-                            DRAFT
-                        </a>
-                    </li>
-                    <li>
-                        <a className="translatedStatusMenu" data-sid={"segment-" + this.props.segment.sid} title="set translated as status"
-                           onClick={this.changeStatus.bind( this, 'translated' )}>
-                            TRANSLATED
-                        </a>
-                    </li>
-                    <li><a className="approvedStatusMenu" data-sid={"segment-" + this.props.segment.sid} title="set approved as status"
-                           onClick={this.changeStatus.bind( this, 'approved' )}>APPROVED</a></li>
+  function overrideSegmentTargetFunctions(SegmentTarget) {
+    let originalGetTargetArea = SegmentTarget.prototype.getTargetArea
+    SegmentTarget.prototype.getTargetArea = function (translation) {
+      if (ReviewImproved && ReviewImproved.enabled() && Review.enabled()) {
+        translation = TagUtils.matchTag(
+          TagUtils.decodeHtmlInTag(
+            TagUtils.decodePlaceholdersToTextSimple(translation),
+          ),
+        )
+        return (
+          <div
+            className="textarea-container"
+            onClick={this.onClickEvent.bind(this)}
+          >
+            <div
+              className="targetarea issuesHighlightArea errorTaggingArea"
+              dangerouslySetInnerHTML={this.allowHTML(translation)}
+            />
+          </div>
+        )
+      } else {
+        return originalGetTargetArea.apply(this, [translation])
+      }
+    }
+  }
 
-                    {!ReviewImproved && config.reviewType !== 'improved' ? (
-                        <li>
-                            <a className="rejectedStatusMenu" data-sid={"segment-" + this.props.segment.sid} title="set rejected as status"
-                               onClick={this.changeStatus.bind( this, 'rejected' )}>
-                                REJECTED
-                            </a>
-                        </li>
-                    ) : (null)}
-
-                    {ReviewImproved || config.reviewType == 'improved' ? (
-                        <li>
-                            <a className="fx" data-sid={"segment-" + this.props.segment.sid} title="set fixed as status"
-                               onClick={this.changeStatus.bind( this, 'fixed' )}>
-                                FIXED
-                            </a>
-                        </li>
-                    ) : (null)}
-                    {ReviewImproved || config.reviewType == 'improved' ? (
-                        <li>
-                            <a className="rb" data-sid={"segment-" + this.props.segment.sid} title="set rebutted as status"
-                               onClick={this.changeStatus.bind( this, 'rebutted' )}>
-                                REBUTTED
-                            </a>
-                        </li>
-                    ) : (null)}
-
-                </ul>
-            } else {
-                return '';
-
-            }
-        };
-
-        const originalbeforeRender = SegmentBody.prototype.beforeRenderOrUpdate;
-        SegmentBody.prototype.beforeRenderOrUpdate = function () {
-            if ( !(ReviewImproved && ReviewImproved.enabled() && Review.enabled()) ) {
-                originalbeforeRender.apply( this )
-            }
-        };
-        const originalAfterRender = SegmentBody.prototype.afterRenderOrUpdate;
-        SegmentBody.prototype.afterRenderOrUpdate = function () {
-            if ( !(ReviewImproved && ReviewImproved.enabled() && Review.enabled()) ) {
-                originalAfterRender.apply( this );
-            }
-        }
+  function overrideTargetButtons(SegmentButtons) {
+    SegmentButtons.prototype.getButtons = function () {
+      let html
+      if (this.props.isReview) {
+        //Revise of Review Improved
+        html = this.getReviewImprovedButtons()
+      } else {
+        //Translate of Review Improved
+        html = this.getReviewImprovedTranslateButtons()
+      }
+      return html
     }
 
-    function overrideSegmentTargetFunctions( SegmentTarget ) {
-        let originalGetTargetArea = SegmentTarget.prototype.getTargetArea;
-        SegmentTarget.prototype.getTargetArea = function ( translation ) {
-            if ( ReviewImproved && ReviewImproved.enabled() && Review.enabled() ) {
-                translation = TagUtils.matchTag(TagUtils.decodeHtmlInTag(TagUtils.decodePlaceholdersToTextSimple(translation)))
-                return <div className="textarea-container" onClick={this.onClickEvent.bind( this )}>
-                    <div className="targetarea issuesHighlightArea errorTaggingArea"
-                         dangerouslySetInnerHTML={this.allowHTML( translation )}/>
-                </div>
-
-            } else {
-                return originalGetTargetArea.apply( this, [translation] )
-            }
-        };
+    SegmentButtons.prototype.getReviewImprovedButtons = function () {
+      let button
+      let currentScore = ReviewImproved.getLatestScoreForSegment(
+        this.props.segment.sid,
+      )
+      if (currentScore == 0) {
+        button = (
+          <li className="right">
+            <a
+              id={'segment-' + this.props.segment.sid + '-button-translated '}
+              onClick={(event) => this.clickOnApprovedButton(event, false)}
+              data-segmentid={'segment-' + this.props.segment.sid}
+              className="approved"
+            >
+              APPROVED
+            </a>
+            <p>{UI.isMac ? 'CMD' : 'CTRL'} ENTER</p>
+          </li>
+        )
+      } else if (currentScore > 0) {
+        button = (
+          <li className="right">
+            <a className="button button-reject">REJECTED</a>
+            <p>{UI.isMac ? 'CMD' : 'CTRL'}+SHIFT+DOWN</p>
+          </li>
+        )
+      }
+      return (
+        <ul
+          className="buttons toggle"
+          data-mount="main-buttons"
+          id={'segment-' + this.props.segment.sid + '-buttons'}
+        >
+          {button}
+        </ul>
+      )
     }
-
-    function overrideTargetButtons( SegmentButtons ) {
-        SegmentButtons.prototype.getButtons = function () {
-            let html;
-            if ( this.props.isReview ) {
-                //Revise of Review Improved
-                html = this.getReviewImprovedButtons()
-            } else {
-                //Translate of Review Improved
-                html = this.getReviewImprovedTranslateButtons()
-            }
-            return html;
-        };
-
-        SegmentButtons.prototype.getReviewImprovedButtons = function () {
-            let button;
-            let currentScore = ReviewImproved.getLatestScoreForSegment(this.props.segment.sid );
-            if ( currentScore == 0 ) {
-                button = <li className="right">
-                    <a id={"segment-" + this.props.segment.sid + "-button-translated "}
-                       onClick={( event ) => this.clickOnApprovedButton( event, false )}
-                       data-segmentid={"segment-" + this.props.segment.sid}
-                       className="approved"
-                    >APPROVED</a>
-                    <p>{(UI.isMac) ? 'CMD' : 'CTRL'} ENTER</p>
-                </li>;
-            } else if ( currentScore > 0 ) {
-                button = <li className="right">
-                    <a className="button button-reject">REJECTED</a>
-                    <p>{(UI.isMac) ? 'CMD' : 'CTRL'}+SHIFT+DOWN</p>
-                </li>;
-            }
-            return <ul className="buttons toggle" data-mount="main-buttons" id={"segment-" + this.props.segment.sid + "-buttons"}>
-                {button}
-            </ul>
-        };
-        SegmentButtons.prototype.getReviewImprovedTranslateButtons = function () {
-            //TODO Remove lokiJs
-            let data = MateCat.db.segments.by( 'sid', this.props.segment.sid );
-            if ( UI.showFixedAndRebuttedButtons( data.status ) ) {
-                return <ul className="buttons toggle" data-mount="main-buttons" id={"segment-" + this.props.segment.sid + "-buttons"}>
-                    <MC.SegmentMainButtons
-                        status={data.status}
-                        sid={data.sid}
-                        segment={this.props.segment.sid}
-                    />
-                </ul>
-            } else {
-                return this.getTranslateButtons()
-            }
-        };
+    SegmentButtons.prototype.getReviewImprovedTranslateButtons = function () {
+      //TODO Remove lokiJs
+      let data = MateCat.db.segments.by('sid', this.props.segment.original_sid)
+      if (UI.showFixedAndRebuttedButtons(data.status)) {
+        return (
+          <ul
+            className="buttons toggle"
+            data-mount="main-buttons"
+            id={'segment-' + this.props.segment.sid + '-buttons'}
+          >
+            <MC.SegmentMainButtons
+              status={data.status}
+              sid={data.sid}
+              segment={this.props.segment.sid}
+            />
+          </ul>
+        )
+      } else {
+        return this.getTranslateButtons()
+      }
     }
+  }
 
-    overrideTranslationIssuesSideButton(TranslationIssuesSideButton);
+  overrideTranslationIssuesSideButton(TranslationIssuesSideButton)
 
-    overrideSegmentTargetFunctions(SegmentTarget);
+  overrideSegmentTargetFunctions(SegmentTarget)
 
-    overrideSegmentBodyFunctions(SegmentBody);
+  overrideSegmentBodyFunctions(SegmentBody)
 
-    overrideTargetButtons(SegmentButtons)
-
-
-})();
+  overrideTargetButtons(SegmentButtons)
+})()
