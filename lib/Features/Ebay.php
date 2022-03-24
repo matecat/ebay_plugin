7e84be764e18d20a650d260bfd15190b78bf0948
Mauro Cassani <maurocassani1978@gmail.com>
2022-03-24 16:08:10 +0100

    removed beforeTMAnalysisCloseProject

--- lib/Features/Ebay.php
+++ lib/Features/Ebay.php
@@ -224,38 +224,6 @@ class Ebay extends BaseFeature {
         return round( $edit_distance );
     }
 
-
-    /**
-     * This function updates the eq_word_count setting it to null right before the project is
-     * closed by the TM Analysis. This way we force the project to always use raw word count.
-     *
-     * @param Projects_ProjectStruct|int $project
-     * 
-     * TODO: this code needs to be refactored, does not works for multiple language jobs
-     */
-    public function beforeTMAnalysisCloseProject( $project ) {
-        $db = \Database::obtain()->getConnection() ;
-
-        $sql_project_id = 'SELECT id FROM jobs WHERE id_project = ?';
-        $stmt = $db->prepare( $sql_project_id );
-
-        $stmt->setFetchMode( \PDO::FETCH_ASSOC );
-
-        if( $project instanceof Projects_ProjectStruct ){
-            $pid = $project->id;
-        } else {
-            $pid = $project;
-        }
-
-        $stmt->execute( [ $pid ] ) ;
-        $result = $stmt->fetch(); //TODO this takes only one job, manage multi language
-
-        $sql = "UPDATE segment_translations SET eq_word_count = null WHERE id_job = ? ";
-        $stmt = $db->prepare( $sql );
-        $stmt->execute( [ $result['id'] ] ) ;
-
-    }
-
     /**
      * If segment is marked as skipped, do no send contribution
      *
